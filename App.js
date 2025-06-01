774c806c625312fb8531c4ddd2c64a4640e570bc
Ahmad Fardous Azimi <fardousazimi@Ahmads-MacBook-Pro.local>
2025-06-01 19:28:46 +1000

    Home Screen Initialised and The structure with the design is build and ready to connect the functionality

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import { useFonts } from 'expo-font';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Trebuc MS': require('./assets/fonts/trebuc.ttf'),
+    'Trebuc MS Italic': require('./assets/fonts/Trebuchet-MS-Italic.ttf'),
+    'Trebuc Bold': require('./assets/fonts/trebuc-bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Loading...</Text></View>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerTitleStyle: {
+          fontFamily: 'Trebuc MS',
+          fontSize: 20,
+        }
+      }} initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Directory" component={StaffDirectory} />
+        <Stack.Screen name="Profile" component={StaffProfile} />
+        <Stack.Screen name="HR Requests" component={HRRequests} />
+        <Stack.Screen name="Intranet" component={Intranet} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +44,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,150 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            {/* Top Section */}
+            <View style={styles.header}>
+                <View>
+                    <Text style={styles.welcome}>Welcome, John Doe</Text>
+                    <Text style={styles.subtitle}>What would you like to do today?</Text>
+                </View>
+                <Image source={require('../assets/icons/profile.png')} style={styles.profileIcon} />
+            </View>
+            {/* Main Content */}
+            <ScrollView>
+                <TouchableOpacity onPress={() => navigation.navigate('announcements')}>
+                    <View style={styles.announcementsContainer}>
+                        <Image source={require('../assets/icons/announcement.png')} style={{ width: 50, height: 50 }} />
+                        <View style={styles.announcements}>
+                            <Text style={styles.announcementsTitle}>Announcments</Text>
+                            <Text style={styles.announcementsSubtitle}>Staff Meeting at 10:00 AM today</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('Directory')}>
+                    <View style={styles.card}>
+                        <Image source={require('../assets/icons/staffDirectory.png')} style={{ width: 50, height: 50 }} />
+                        <Text style={{ fontFamily: "Trebuc MS", fontSize: 16 }}>Staff Directory</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('Intranet')}>
+                    <View style={styles.card}>
+                        <Image source={require('../assets/icons/intranet.png')} style={{ width: 50, height: 50 }} />
+                        <Text style={{ fontFamily: "Trebuc MS", fontSize: 16 }}>Intranet</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('HR Requests')}>
+                    <View style={styles.card}>
+                        <Image source={require('../assets/icons/hrRequest.png')} style={{ width: 50, height: 50 }} />
+                        <Text style={{ fontFamily: "Trebuc MS", fontSize: 16 }}>HR Requests</Text>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
+            {/* Bottom Section */}
+            <View style={styles.bottomNav}>
+                <TouchableOpacity>
+                    <Image source={require('../assets/icons/home.png')} style={styles.navIcon} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image source={require('../assets/icons/staff.png')} style={styles.navIcon} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image source={require('../assets/icons/hr.png')} style={styles.navIcon} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image source={require('../assets/icons/menu.png')} style={styles.navIcon} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        padding: 20,
+
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    welcome: {
+        fontSize: 28,
+        color: '#941A1D',
+        fontFamily: "Trebuc Bold"
+    },
+    subtitle: {
+        marginTop: 5,
+        fontSize: 14,
+        color: '#595959',
+        fontFamily: "Trebuc MS"
+    },
+    profileIcon: {
+        width: 50,
+        height: 50,
+    },
+    announcementsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20,
+        backgroundColor: '#941A1D',
+        padding: 15,
+        marginBottom: 20,
+        borderRadius: 30,
+        height: 110,
+
+    },
+    announcementsTitle: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: '#ffffff',
+        fontFamily: "Trebuc Bold",
+        textAlign: 'center',
+    },
+    announcementsSubtitle: {
+        fontSize: 14,
+        color: '#ffffff',
+        fontFamily: "Trebuc MS",
+        textAlign: 'center',
+    },
+    card: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#D9D9D9',
+        padding: 15,
+        borderRadius: 30,
+        marginBottom: 20,
+        fontSize: 16,
+        fontFamily: "Trebuc MS",
+        height: 110,
+    },
+    bottomNav: {
+        position: 'absolute',
+        height: 85,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: '#262626',
+        paddingBottom: 20,
+        paddingTop: 12,
+        paddingHorizontal: 24,
+
+    },
+    navIcon: {
+        width: 25,
+        height: 25,
+    },
+
+});
